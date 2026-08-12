c571287909712f2c2c2788b46e96366a51896209
112415168-create <fullugupta1234@gmail.com>
2026-08-12 23:39:40 +0530

    Add frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
