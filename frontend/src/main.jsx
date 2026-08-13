fcc984bb51661ad394b8541115c00139a3a6557b
112415168-create <fullugupta1234@gmail.com>
2026-08-13 23:23:02 +0530

    Update frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import {Provider} from 'react-redux'
+import {GoogleOAuthProvider} from '@react-oauth/google'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+import store from './store.js'
+
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+axios.interceptors.request.use(
+  (response)=>response,
+  (error)=>{
+    if(error.response && error.response.status === 401){
+      localStorage.removeItem('user');
+      window.location.href='/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <Provider store={store}>
+        <Router>
+          <App/>
+        </Router>
+      </Provider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
